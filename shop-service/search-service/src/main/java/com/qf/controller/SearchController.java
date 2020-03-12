4485fe2ce3e91fb877ab8b0f2fb1ee0bc2d1c8db
lxd6666 <870723944@qq.com>
2020-03-12 22:07:52 +0800

    完成搜索接口

--- shop-service/search-service/src/main/java/com/qf/controller/SearchController.java
+++ shop-service/search-service/src/main/java/com/qf/controller/SearchController.java
@@ -1,9 +1,35 @@
 package com.qf.controller;
 
+import com.qf.dto.ResultBean;
+import com.qf.service.SearchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("search")
 public class SearchController {
+    @Autowired
+    private SearchService searchService;
+
+    @RequestMapping("query")
+    public String searchByKeyword(@PathVariable String keyword, Model model){
+
+        ResultBean resultBean = searchService.searchByKeyword(keyword);
+
+        //List<TProductSearchDTO>
+        model.addAttribute("products",resultBean.getData());
+         return "search";
+    }
+
+
+    @RequestMapping("addProduct")
+    public ResultBean addProduct(@PathVariable Long pid){
+
+        return searchService.addProduct(pid);
+
+
+    }
 }
--- shop-web/search-web/src/main/java/com/qf/service/SearchService.java
+++ shop-web/search-web/src/main/java/com/qf/service/SearchService.java
@@ -1,7 +1,17 @@
 package com.qf.service;
 
+import com.qf.dto.ResultBean;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient(value = "SEARCH-SERVICE")
 public interface SearchService {
+
+    @RequestMapping("search/query")
+     String searchByKeyword(@PathVariable String keyword);
+
+    @RequestMapping("search/addProduct")
+    ResultBean addProduct(@PathVariable Long pid);
 }
