a68f3c741afc3aea0bf1cfc5874e77682c6140fc
hemin <1984491452@qq.com>
2020-03-17 20:02:09 +0800

    3.17 何敏 one

--- shop-web/shop-index-web/src/main/java/com/qf/shop/index/web/controller/IndexController.java
+++ shop-web/shop-index-web/src/main/java/com/qf/shop/index/web/controller/IndexController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,7 +39,14 @@ public class IndexController {
         return "index";
     }
 
-    public ResultBean checkIsLogin(){
+    /**
+     * 检查是否已登录
+     * @param uuid
+     * @return
+     */
+    @RequestMapping("checkIsLogin")
+    @ResponseBody
+    public ResultBean checkIsLogin(@CookieValue(name = RedisConstant.USER_LOGIN_PRE,required = false) String uuid){
         return  null;
     }
 
